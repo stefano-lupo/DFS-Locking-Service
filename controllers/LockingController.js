17f8ca8c34181c82afed2d9b9afc3c56d3d37dd7
Stefano <lupos@tcd.ie>
2017-11-25 20:52:34 +0000

    Added authentication / decryption in compliance with security service

--- controllers/LockingController.js
+++ controllers/LockingController.js
@@ -6,26 +6,24 @@ import moment from 'moment';
  * Attempts to acquire a lock for the file
  */
 const lockFile = (req, res) => {
-  let lockedFiles = req.app.get('lockedFiles');
 
-  // TODO: Add Auth as header
   const { _id } = req.params;
-  const { email } = req.query;
+  const { clientId } = req;
 
-  //TODO Verify email with Directory Service?
-  if(!_id || !email) {
-    return res.status(409).send({granted: false, message: `Invalid fields - email: ${email}, _id: ${_id}`});
+  if(!_id) {
+    return res.status(409).send({granted: false, message: `No file _id requested for lock`});
   }
+
+  let lockedFiles = req.app.get('lockedFiles');
   const { secret, expiry } = req.app.get('jwt');
+
   const lock = lockedFiles.get(_id);
-  if(!lock || lock.expiresAt.isBefore(moment()) ||lock.email === email) {
-    // Give Lock
-    const lock = jwt.sign({data: {email, _id}}, secret, {expiresIn: `${expiry}m`});
-    lockedFiles.set(_id, {email, expiresAt: moment().add(expiry, 'm')});
+  if(!lock || lock.expiresAt.isBefore(moment()) || lock.clientId === clientId) {
+    // File is not locked, lock expired or lock already belongs to client (extend)
+    const lock = jwt.sign({data: {clientId, _id}}, secret, {expiresIn: `${expiry}m`});
+    lockedFiles.set(_id, {clientId, expiresAt: moment().add(expiry, 'm')});
     res.send({granted: true, message: `Lock expires in ${expiry}m`, lock});
-    console.log("LOCK GIVEN");
   } else {
-    console.log("LOCK NOT GIVEN");
     res.send({granted: false, message: `Lock is in use - try again later`});
   }
 };
@@ -45,41 +43,45 @@ const unlockFile = (req, res) => {
  * Checks if a token is valid (requested by File System Nodes)
  */
 const validateLock = (req, res) => {
-  const { lock, email, _id } = req.body;
-  if(!lock || !email || !_id) {
+  const { clientId } = req;
+  const { lock, _id } = req.body;
+  if(!lock) {
     return res.status(409).send({
       valid: false,
-      message: `Invalid fields - email: ${email}, lock: ${lock}, _id: ${_id}`
+      message: `No Lock provided`
     });
   }
-  res.send(isLockValid(lock, email, _id, req.app.get('jwt')));
+  res.send(isLockValid(lock, _id, clientId, req.app.get('jwt')));
 };
 
 
 /**
  * Helper function to check if lock is valid
  * @param lock the JWT
- * @param email the user who apparently received the lock
+ * @param clientId the user who apparently received the lock
  * @param _id the _id of the apparent file they locked
  * @param jwtParams jwtSecret and expiry
  * @returns {valid: boolean, message: String}
  */
-function isLockValid(lock, email, _id, jwtParams) {
+function isLockValid(lock, _id, clientId, jwtParams) {
   const { secret } = jwtParams;
   try {
     let decoded = jwt.verify(lock, secret);
     const { exp, data } = decoded;
 
-
+    // Check the lock was actually acquired for this file
     if(data._id !== _id) {
       return {
         valid: false,
         message: `Token not issued for file ${_id}`
       }
-    } else if(data.email !== email) {
+    }
+
+    // Check the lock was actually acquired by this client
+    else if(data.clientId !== clientId) {
       return {
         valid: false,
-        message: `Token not issued to ${email}`
+        message: `Token not issued to ${clientId}`
       }
     }
 
@@ -89,13 +91,18 @@ function isLockValid(lock, email, _id, jwtParams) {
     }
 
   } catch(err) {
+    // Lock couldn't be verified
     return {
       valid: false,
       message: err.message
     }
   }
-
 }
+
+
+
+
+
 
 module.exports = {
   lockFile,
